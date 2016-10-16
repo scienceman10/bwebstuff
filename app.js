754da63bd3a9e2cf19dd270e45a18e017c8038bf
Marek Newton <marek.s.newton@gmail.com>
2016-10-16 17:17:18 -0600

    first commit redo

--- app.js
+++ app.js
@@ -14,7 +14,6 @@ const session = require('express-session');
 const parseurl = require('parseurl');
 
 //more imports go here
-app.set("port", process.env.PORT || 3000);
 app.use(express.static(__dirname + "/public"));
 //end of setup
 
@@ -116,6 +115,6 @@ app.use((err, req, res, next)=>{
   res.render("500");
 })
 
-app.listen(3000, function () {
+app.listen(3000 || process.env.PORT, function () {
   console.log('app started');
 });
